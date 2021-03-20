09e346c76c82d22bddbed186847dd0cbd6153d9a
Erik Huang <ekhu94@gmail.com>
2021-03-20 07:55:52 -0700

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const renderedDirectors = directors.map(d => {
+    const renderedMovies = d.movies.map(m => {
+      return <li key={m}>{m}</li>
+    });
+    return (
+      <div key={d.name}>
+        <h4>{d.name}</h4>
+        <ul>
+          {renderedMovies}
+        </ul>
+      </div>
+    );
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderedDirectors}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderedMovies = movies.map(m => {
+    const renderedGenres = m.genres.map(g => {
+      return <li key={g}>{g}</li>
+    });
+    return (
+      <div key={m.title}>
+        <h4>{m.title}</h4>
+        <p>Running Time: {m.time}</p>
+        <ul>
+          {renderedGenres}
+        </ul>
+      </div>
+    );
+  });
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderedMovies}
     </div>
   );
 };
